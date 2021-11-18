7816cc4d72d3c47e3c079e6926c14de1f5a03fb2
matthew <imatthewkang@gmail.com>
2021-11-18 13:09:46 -0800

    templates

--- python_website/urls.py
+++ python_website/urls.py
@@ -14,13 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('about', views.about, name="about"),
-    path('contact', views.contact, name="contact")
+    path("admin/", admin.site.urls),
+    path("", include('python_website.apps.public.urls')),
+    path("accounts/profile", views.ProfileView.as_view(), name="profile"),
+    # django auth
+    path(
+        "accounts/login",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path("accounts/logout", auth_views.LogoutView.as_view(), name="logout")
+    # path('accounts/', include('django.contrib.auth.urls'))
 ]
--- python_website/views.py
+++ python_website/views.py
@@ -1,14 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def index(request):
+
+def index(request: HttpRequest) -> HttpResponse:
     # template = loader.get_template('index.html')
     # return HttpResponse(template.render({}, request))
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
-def about(request):
-    return render(request, 'about.html')
 
-def contact(request):
-    return render(request, 'contact.html')
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, "about.html")
+
+
+def contact(request: HttpRequest) -> HttpResponse:
+    return render(request, "contact.html")
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/profile.html"
